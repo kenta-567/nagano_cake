a49af5e8e841935636dc415142a3cdf1888f6e99
EC2 Default User <ec2-user@ip-172-31-87-79.ec2.internal>
2021-01-24 14:58:06 +0000

    [Add] データベース追加

---
+++ app/models/order.rb
@@ -0,0 +1,19 @@
+class Order < ApplicationRecord
+  
+  belongs_to :customer
+  has_many :order_details, dependent: :destroy
+
+
+  enum payment_method:{
+    credit: 1,
+    bank: 2
+  }
+
+  enum status:{
+    payment_waiting: 0,
+    payment_confirmation: 1,
+    in_production: 2,
+    preparing_delivery: 3,
+    delivered: 4
+  }
+end
---
+++ app/models/order_detail.rb
@@ -0,0 +1,13 @@
+class OrderDetail < ApplicationRecord
+  
+  belongs_to :order
+  belongs_to :item
+
+
+enum making_status:{
+    payment_waiting: 0,
+    payment_confirmation: 1,
+    in_production: 2,
+    preparing_delivery: 3
+  }
+end
