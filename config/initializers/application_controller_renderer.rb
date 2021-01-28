bc42dca66bfeabdc5a6c3d8f6eb9382bf47c64ac
EC2 Default User <ec2-user@ip-172-31-87-79.ec2.internal>
2021-01-28 13:44:44 +0000

    [Add] item\new,index,show ページ一部編集

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '522f42e4ef2e30a0c82fdbbf4e9d1eaffae06559c4329c249480be41657b43aa8f6197f47f05c9b14a19edf36ec69960f95e64a208ad3f9d5f5fc608a7d6c059'
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,16 @@ Admin.create!(
    email: 'test@test.com',
    password: '000000'
 )
+
+Customer.create!(
+   last_name: '現実',
+   first_name: '理想',
+   last_name_kana: 'ゲンジツ',
+   first_name_kana: 'コレガ',
+   email: 'ooo@ooo.com',
+   encrypted_password: '111111',
+   postal_code: '5551111',
+   address: '東京都渋⾕区神南１丁⽬１９−１１ パークウェースクエア24階',
+   telephone_number: '0368696935',
+   is_active: 'true'
+   )
